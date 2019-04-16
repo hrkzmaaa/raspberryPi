57e2d8662b1f5faca157938378dbe516be1793aa
KAZUMA-HIRAKAWA <k.hirakazu.0611@gmail.com>
2019-04-16 16:59:25 +0900

    コメント

--- beaconCatch.js
+++ beaconCatch.js
@@ -4,13 +4,20 @@ const axiosBase = require('axios')
 Bleacon.startScanning()
 Bleacon.on('discover', function(bleacon) {
   // immediateのときにエンドポイントに送信
+  // immediate : 10cm以内
+  // near : 1m以内
+  // far : それ以上
   if (bleacon.proximity === 'immediate') {
     console.log(bleacon)
     sendBeaconInfo(bleacon)
   }
 })
 
-const sendBeaconInfo = async bleacon => {
+/**
+ * ビーコン情報をAWS上に送信する
+ * @param {*} bleacon ビーコンの情報（JSON）
+ */
+const sendBeaconInfo = async beacon => {
   const axios = axiosBase.create({
     baseURL: 'https://r2hw56el75.execute-api.ap-northeast-1.amazonaws.com/test', // APIGatewayのエンドポイント
     headers: {
@@ -20,5 +27,5 @@ const sendBeaconInfo = async bleacon => {
     responseType: 'json'
   })
 
-  await axios.put('/', bleacon)
+  await axios.put('/', beacon)
 }
