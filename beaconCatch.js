4e80b438e30d15fe8240db3313633673a04408e3
KAZUMA-HIRAKAWA <k.hirakazu.0611@gmail.com>
2019-04-16 14:51:17 +0900

    デバッグ

--- beaconCatch.js
+++ beaconCatch.js
@@ -1,6 +1,8 @@
 const Bleacon = require("bleacon");
 
 Bleacon.startScanning();
-let beacon = Bleacon.on("discover", function (bleacon) {});
+Bleacon.on("discover", function (bleacon) {
 
-console.log(beacon);
+  console.log(bleacon);
+
+});
