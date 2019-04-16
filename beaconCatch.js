65ecfd465b741efe5df77eedbd12fcf7d9c01388
KAZUMA-HIRAKAWA <k.hirakazu.0611@gmail.com>
2019-04-16 15:19:37 +0900

    できた説

--- beaconCatch.js
+++ beaconCatch.js
@@ -1,13 +1,24 @@
-const Bleacon = require("bleacon");
+const Bleacon = require('bleacon')
 
-Bleacon.startScanning();
-Bleacon.on("discover", function (bleacon) {
-
-  console.log(bleacon.proximity);
+Bleacon.startScanning()
+Bleacon.on('discover', function(bleacon) {
 
   // immediateのときにエンドポイントに送信
   if (bleacon.proximity === 'immediate') {
-    
+    console.log(bleacon)
+    sendBeaconInfo();
   }
+})
 
-});
+async const sendBeaconInfo = () => {
+  const axios = axiosBase.create({
+    baseURL: 'https://r2hw56el75.execute-api.ap-northeast-1.amazonaws.com/test', // APIGatewayのエンドポイント
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest'
+    },
+    responseType: 'json'
+  })
+
+  await axios.put('/', bleacon);
+}
